9afbcee389db6ef9cb3933269da1f2a363ae69e6
GavinCopley <gbcopley@gmail.com>
2025-06-10 11:29:45 -0700

    added vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: vercel()
+  }
 };
 
 export default config;
