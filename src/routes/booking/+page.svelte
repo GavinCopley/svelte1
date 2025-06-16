6ecf8e243248a6b1a82f93bbbfd1b571c204d5c3
GavinCopley <gbcopley@gmail.com>
2025-06-15 22:30:19 -0700

    changed calendar a lil on booking

--- src/routes/booking/+page.svelte
+++ src/routes/booking/+page.svelte
@@ -2,6 +2,8 @@
     import Calendar from '$lib/components/calendar-20.svelte';
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-12">
-    <Calendar />
+<div class="flex justify-center mb-12" style="margin-top: 120px;">
+    <div style="transform: scale(1.2) !important;">
+        <Calendar />
+    </div>
 </div>
